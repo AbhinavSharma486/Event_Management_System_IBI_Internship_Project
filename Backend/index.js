9958d056ccd95cb08df6a81e00a4182c12c977d9
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-06-29 00:36:07 +0530

    backend is now running on 5001 port

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
