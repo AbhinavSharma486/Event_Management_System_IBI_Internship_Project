8e16eb33f5902dbe452dc301103b57f1143b27cb
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-06-30 11:26:02 +0530

    added user schema

---
+++ Backend/models/User.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    },
+    profilePic: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
+    createdEvents: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event'
+    }],
+    eventAttendees: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event'
+    }]
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
