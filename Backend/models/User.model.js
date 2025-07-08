8500e97d4154e9d4cd1dfacaf93b02d7d5dcd216
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-07-08 23:29:10 +0530

    update user model

--- Backend/models/User.model.js
+++ Backend/models/User.model.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength: 6
     },
+    mobileNumber: {
+      type: String,
+      required: true,
+      trim: true
+    },
     profilePic: {
       type: String,
       default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
